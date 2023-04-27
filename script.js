d0dfbc0d1b2113f2433bc8d272f9667a453568db
Rakesh Kmar S <srk00100@yahoo.com>
2023-04-27 11:59:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// intro 
+
+/* // window.alert("This is a Learner website");
+console.log("My first statement in JS");
+console.log("I am here to learn JS");
+ */
+/* let firstname = "Brocode";
+let age = 11;
+let enrollment = true;
+
+console.log("firstname ", firstname);
+console.log("age ", age);
+console.log("Enrollment ", enrollment);
+
+document.getElementById("p1").innerHTML = "Hello " + firstname;
+document.getElementById("p2").innerHTML = "your age is : "+ age;
+document.getElementById("p3").innerHTML = "Enrollment "+ enrollment;
+ */
+
+//Arthematic OPerations
+
+/* let students = 20;
+
+// students = students + 1;
+// students = students-1;
+// students=students*3;
+// students=students/2;
+
+console.log(students);
+ */
+
+//user input
+
+/* 
+How to accept user input 
+1 Easy way with window prompt
+2 difficult way with HTML Box */
+/* 
+let username = window.prompt("what's your name");
+console.log(username); */
+/* let username;
+document.getElementById("button").onclick = function () {
+    username = document.getElementById("mytext").value;
+    console.log(username);
+    document.getElementById("mylabel").innerHTML = "Hello " + username;
+}
+ */
+
+// type conversion : changing one type to another(Strings, number, boolean)
+
+/* let age = window.prompt("How old are you?");
+console.log(typeof age);
+age = Number(age);
+console.log(typeof age);
+age += 1;
+console.log("Happy birthday now your's age is ", age); */
+/* let x = Number("3.14");
+console.log(x, typeof x);
+
+let y = String(3.14);
+console.log(y, typeof y);
+
+let z = Boolean("");
+console.log(z, typeof z);
+
+let v = Boolean("Hello");
+console.log(v, typeof v); */
+
+// const = a varible cant be changed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
